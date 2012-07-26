65a8560967b33b99e454e9b04bf0314b8cb2f30b
David Khoo <davidk@bii.a-star.edu.sg>
2012-07-26 11:28:56 +0800

    Version 0.03 - TIMING - updated with more runtimes - cluster.h - matrix size definitions moved there

--- clustering/cluster.h
+++ clustering/cluster.h
@@ -1,6 +1,15 @@
 //      cluster.h
 
+#define N_SCANS 500
+#define N_MZPOINTS 100000
+
 typedef struct {
 	double mz, I;
 	int *cluster_flag; // Pointer to cluster point belongs to (NULL if none)
 } Point;
+
+// Print error message and abort program with error exit value
+int infox (const char *errmsg, int exitval) {
+	fprintf(stderr, "%s\nExiting at %s:%d.\n", errmsg, __FILE__, __LINE__);
+	exit(exitval);
+}
--- clustering/clustermatrix.c
+++ clustering/clustermatrix.c
@@ -4,15 +4,14 @@
 #include "cluster.h"
 
 #define BUFLEN 300
-#define N_SCANS 500
-#define N_MZPOINTS 100000
 
+// Allocate pointmatrix
 Point* pointmatrix(int dim1, int dim2) {
 	Point *matrix;
 	
 	matrix = malloc(dim1*dim2, sizeof(Point));
 	if (matrix == NULL)
-		infox ("Couldn't malloc pointmatrix.", -1);
+		infox ("Couldn't init pointmatrix.", -1);
 	
 	return matrix;
 }
@@ -21,11 +20,25 @@ int main(int argc, char** argv)
 {
 	FILE *infile;
 	char line [BUFLEN] = {'\0'};
-	Point* points;
+	Point *points;
+	unsigned int scan_index = 0;
+	unsigned int mz_index = 0;
 	
 	points = pointmatrix(N_SCANS, N_MZPOINTS);
 	
+	infile = fopen(argv[1],"r");
+	while(fgets(line,BUFLEN,infile)!=NULL) {
+		double curr_RT = 0;
+		double RT, mz, I;
+		int ret = sscanf (line, " %*d  %lf  %lf  %lf", &RT, &mz, &I);
+		if (ret == 3) {
+			// Assume that points in CSV are already sorted by RT
+			
+		}
+	}
+	fclose(infile);
 	
 	free(points);
+	
 	return 0;
 }
