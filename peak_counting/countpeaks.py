14f8f9cf2ca9c729c2a332abb7afbe00bb94ba2e
David Khoo <davidk@bii.a-star.edu.sg>
2012-08-21 13:05:34 +0800

    Version 0.5.7.5 - countpeaks.py now takes filename globs instead of directories

--- peak_counting/countpeaks.py
+++ peak_counting/countpeaks.py
@@ -5,10 +5,10 @@
 #       
 
 import sys
-import os
-import fnmatch
+import glob
 import re
 import numpy as np
+from os.path import basename
 from sklearn.cluster import MeanShift
 
 # Tuning parameters
@@ -17,25 +17,27 @@ sc = 50   # Scaling parameter for RT
 
 def main():
 	r = re.compile('(\d+)\s+(\d+\.\d+)\s+(\d+\.\d+)\s+(\d+\.\d+)')
+	fr = re.compile('\d{6}\.clust')
 	ms = MeanShift(bandwidth=bw, bin_seeding=True)
-	paths = [p for p in sys.argv[1:] if os.path.exists(p)]
-	if not paths:
-		print "Usage: "+os.path.basename(__file__)+" <cluster directory> ..."
+	files = []
+	for g in sys.argv[1:]:
+		files += glob.glob(g)
+	files = [f for f in files if fr.search(basename(f))]
+	if not files:
+		print 'Usage: ' + basename(__file__) + ' <cluster> ...'
 		return -1
-	for path in paths:
-		for subdir, dirs, files in os.walk(path):
-			for file in sorted(fnmatch.filter(files,'*.clust')):
-				f=open(os.path.join(subdir,file), 'r')
-				lines=f.readlines()
-				f.close()
-				pts = []
-				for line in lines:
-					res=r.search(line)
-					pts.append([float(res.group(2))/sc, float(res.group(3))])
-				ms.fit(np.array(pts))
-				labels_unique = np.unique(ms.labels_)
-				n_clusters_ = len(labels_unique)
-				print os.path.join(subdir,file) + ": %d" % n_clusters_
+	for file in files:
+		f = open(file, 'r')
+		lines = f.readlines()
+		f.close()
+		pts = []
+		for line in lines:
+			res = r.search(line)
+			pts.append([float(res.group(2))/sc, float(res.group(3))])
+		ms.fit(np.array(pts))
+		labels_unique = np.unique(ms.labels_)
+		n_clusters_ = len(labels_unique)
+		print file + ': %d' % n_clusters_
 	return 0
 
 if __name__ == '__main__':
