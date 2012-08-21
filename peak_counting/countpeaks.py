595281a433607ce6963d1020cf930de5202ea85b
David Khoo <davidk@bii.a-star.edu.sg>
2012-08-21 13:20:48 +0800

    Version 0.5.7.6 - Code reorganization and more specific regexp for countpeaks.py

--- peak_counting/countpeaks.py
+++ peak_counting/countpeaks.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-#       countpeaks.py
-#       
+# countpeaks.py
 
 import sys
 import glob
@@ -16,24 +15,23 @@ bw = 0.15 # Bandwidth for MeanShift
 sc = 50   # Scaling parameter for RT
 
 def main():
-	r = re.compile('(\d+)\s+(\d+\.\d+)\s+(\d+\.\d+)\s+(\d+\.\d+)')
-	fr = re.compile('\d{6}\.clust')
-	ms = MeanShift(bandwidth=bw, bin_seeding=True)
 	files = []
 	for g in sys.argv[1:]:
 		files += glob.glob(g)
-	files = [f for f in files if fr.search(basename(f))]
+	files = [f for f in files if re.match('^\d{6}\.clust$', basename(f))]
 	if not files:
 		print 'Usage: ' + basename(__file__) + ' <cluster> ...'
 		return -1
+	lre = re.compile('(\d+)\s+(\d+\.\d+)\s+(\d+\.\d+)\s+(\d+\.\d+)')
+	ms = MeanShift(bandwidth=bw, bin_seeding=True)
 	for file in files:
-		f = open(file, 'r')
-		lines = f.readlines()
-		f.close()
 		pts = []
-		for line in lines:
-			res = r.search(line)
-			pts.append([float(res.group(2))/sc, float(res.group(3))])
+		f = open(file, 'r')
+		for line in f:
+			res = lre.search(line)
+			if res:
+				pts.append([float(res.group(2))/sc, float(res.group(3))])
+		f.close()
 		ms.fit(np.array(pts))
 		labels_unique = np.unique(ms.labels_)
 		n_clusters_ = len(labels_unique)
