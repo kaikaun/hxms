b5ec2dbd2a0ff200461de7048552c1c7f20f0802
David Khoo <davidk@bii.a-star.edu.sg>
2012-10-03 16:13:52 +0800

    Changed correct_cluster.py to take a file containing a list of filenames instead

--- 03_deadtime_correction/python/correct_cluster.py
+++ 03_deadtime_correction/python/correct_cluster.py
@@ -4,7 +4,6 @@
 # optimize_dt.py
 
 import argparse
-import glob
 from os.path import basename
 import re
 import math
@@ -37,19 +36,19 @@ def correct_deadtime(dt, pulses, prop, spectrum):
 
 def main():
 	parser = argparse.ArgumentParser(description='Correct clusters for deadtime.')
-	parser.add_argument('clusters', metavar='C', nargs='+',
-						help='cluster filenames (including wildcards)')
+	parser.add_argument('clusters', 
+						help='file containing cluster filenames')
 	parser.add_argument('-d','--ext', dest='ext', type=float, default=5., 
 						help='extending deadtime (ns)')
 	parser.add_argument('-D','--non', dest='non', type=float, default=0., 
 						help='non-extending deadtime (ns)')
 	parser.add_argument('-s','--scantime', dest='ST', type=float, default=0.23, 
 						help='scan time (s)')
-	parser.add_argument('-c','--cycletime', dest='CT', type=float, default=45, 
+	parser.add_argument('-c','--cycletime', dest='CT', type=float, default=45., 
 						help='cycletime (μs)')
-	parser.add_argument('-l','--length', dest='L', type=float, default=1078, 
+	parser.add_argument('-l','--length', dest='L', type=float, default=1078., 
 						help='effective TOF path length (mm)')
-	parser.add_argument('-v','--voltage', dest='V', type=float, default=5630, 
+	parser.add_argument('-v','--voltage', dest='V', type=float, default=5630., 
 						help='effective accelerating voltage (V)')
 	args = parser.parse_args()
 
@@ -57,32 +56,35 @@ def main():
 	pulses = args.ST / (args.CT * 1E-6)
 	prop = (args.L * 1E-3) / math.sqrt(2 * 96485333.7 * args.V)
 
-	files = [glob.glob(g) for g in args.clusters]
-	files = [f for f in sum(files,[]) if re.match('\d{6}\.clust$', basename(f))]
-	if not files:
-		print 'No valid cluster files found!'
+	filenames = args.clusters
+	try:
+		with open(filenames, 'r') as inf:
+			files = sum([line.rstrip().split(" ") for line in inf],[])
+	except IOError:
+		print "error opening ", filenames
 		return -1
 
 	lre = re.compile('(\d+)\s+(\d+\.\d+)\s+(\d+\.\d+)\s+(\d+\.\d+)')
 	for file in files:
 		spectrum={}
 		scannums={}
-		f = open(file, 'r')
-		for line in f:
-			res = lre.search(line)
-			if res:
-				RT = float(res.group(2))
-				if RT not in spectrum:
-					spectrum[RT] = {}
-				spectrum[RT][float(res.group(3))] = float(res.group(4))
-				scannums[RT] = res.group(1)
-		f.close()
+		try:
+			with open(file, 'r') as f:
+				for line in f:
+					res = lre.search(line)
+					if res:
+						RT = float(res.group(2))
+						if RT not in spectrum:
+							spectrum[RT] = {}
+						spectrum[RT][float(res.group(3))] = float(res.group(4))
+						scannums[RT] = res.group(1)
+		except IOError:
+			next
 		spectrum = correct_deadtime(dt,pulses,prop,spectrum)
-		f = open(file + '.corr', 'w')
-		for RT, scan in sorted(spectrum.iteritems()):
-			for mz, I in sorted(scan.iteritems()):
-				f.write("%s %.3f %.3f %.3f\n" % (scannums[RT],RT,mz,I))
-		f.close()
+		with open(file + '.corr', 'w') as f:
+			for RT, scan in sorted(spectrum.iteritems()):
+				for mz, I in sorted(scan.iteritems()):
+					f.write("%s %.3f %.3f %.3f\n" % (scannums[RT],RT,mz,I))
 
 	return 0
 
